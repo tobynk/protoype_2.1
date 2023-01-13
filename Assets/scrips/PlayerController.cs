524b8cc55149f7bc08c8c15bdec88495ecd72e47
tobynk <122053994+tobynk@users.noreply.github.com>
2023-01-13 12:34:01 -0800

    2.4 Destroy objects on collision
    up to 2.4 on create with code

--- Assets/scrips/PlayerController.cs
+++ Assets/scrips/PlayerController.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_contorler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xRange = 20;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
